ee4debfc5e48c2d95507bf6e30ee61db68ce221d
Dean-ZhenYao-Wang <137473325@qq.com>
2022-05-14 12:41:52 +0800

    添加项目文件。

---
+++ BlazorApp1/Task_DataDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp1
+{
+    public class Task_DataDTO:BlazorGantt.Task_Data
+    {
+    }
+    public class Task_LinkDTO : BlazorGantt.Task_Link
+    {
+
+    }
+}
---
+++ BlazorGantt/wwwroot/GanttLoader.js
@@ -0,0 +1,7 @@
+﻿import "./analytics.js"
+import "./raven.min.js"
+import "./gantt.js"
+
+export function set_config_date_format(date_format) {
+    gantt.config.date_format = date_format;
+}
